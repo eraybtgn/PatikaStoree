3ebe725e58c62d5bdda2fe2e9487c42b3baee5e8
erayBatigun <eraybtgn24@gmail.com>
2022-11-20 18:25:06 +0300

    Second commit.

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,179 @@
 import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        MarkaModel phone2 = new MarkaModel(123,"Samsung","Samsung S22",16999,10,1000,128,8,5000,6.1,"Mavi");
-        MarkaModel noteBook1 = new MarkaModel(1234,"Lenovo","Idea pad",12000,15,250,256,8,51000,15.6,"Gümüş");
-        MarkaModel phone1 = new MarkaModel(12,"Apple","İphone 14",30999,0,100,128,6,3215,6.1,"Siyah");
-        MarkaModel noteBook2 = new MarkaModel(12345,"Huawei","Matebook",12000,15,250,256,8,51000,15.6,"Gümüş");
-        MarkaModel noteBook3 = new MarkaModel(123456,"Casper","Excalibur",18000,0,200,512,16,49000,15.6,"Siyah");
-        MarkaModel noteBook4 = new MarkaModel(1234567,"Asus","TUF GAMİNG",18000,0,200,512,16,49000,15.6,"Siyah");
-        MarkaModel noteBook5 = new MarkaModel(12345678,"HP","VİCTUS",18000,0,200,512,16,49000,15.6,"Siyah");
-        MarkaModel phone3 = new MarkaModel(123456789,"Xiaomi","Mİ 12",18000,0,200,128,8,4900,6.7,"Siyah");
-        MarkaModel noteBook6 = new MarkaModel(1234567890,"Monster","Abra a5 V15.2",18000,10,1000,512,16,48000,15.6,"Siyah");
+        List<Model> list = new ArrayList<Model>();
 
-        int choice;
+        Model phone2 = new Model(123, "Samsung", "Samsung S22","Phone", 16999, 10, 1000, 128, 8, 5000, 6.1, "Mavi");
+        Model noteBook1 = new Model(1234, "Lenovo", "Idea pad","Notebook", 12000, 15, 250, 256, 8, 51000, 15.6, "Gümüş");
+        Model phone1 = new Model(12, "Apple", "İphone 14","Phone", 30999, 0, 100, 128, 6, 3215, 6.1, "Siyah");
+        Model noteBook2 = new Model(12345, "Huawei", "Matebook","NoteBook", 12000, 15, 250, 256, 8, 51000, 15.6, "Gümüş");
+        Model noteBook3 = new Model(123456, "Casper", "Excalibur","Notebook", 18000, 0, 200, 512, 16, 49000, 15.6, "Siyah");
+        Model noteBook4 = new Model(1234567, "Asus", "TUF GAMİNG","Notebook" ,18000, 0, 200, 512, 16, 49000, 15.6, "Siyah");
+        Model noteBook5 = new Model(12345678, "HP", "VİCTUS","Notebook", 18000, 0, 200, 512, 16, 49000, 15.6, "Siyah");
+        Model phone3 = new Model(123456789, "Xiaomi", "Mİ 12","Notebook", 18000, 0, 200, 128, 8, 4900, 6.7, "Siyah");
+        Model noteBook6 = new Model(1234567890, "Monster", "Abra a5 V15.2","Notebook", 18000, 10, 1000, 512, 16, 48000,15.6, "Siyah");
+
+        list.add(noteBook1);
+        list.add(noteBook2);
+        list.add(noteBook3);
+        list.add(noteBook4);
+        list.add(noteBook5);
+        list.add(noteBook6);
+        list.add(phone1);
+        list.add(phone2);
+
+        list.add(phone3);
+        /*
+         * // new Model
+         * list.add(new Model(0, null, null, 0, 0, 0, 0, 0, 0, 0, null));
+         * // Model silme
+         * list = removeModel(123, list);
+         *
+         * // ıd ye göre getirme
+         * getId(1, list);
+         * // markaya göre getirme
+         * List<Model> markaList = new ArrayList<>();
+         * markaList= getMarka("apple", markaList);
+         */
         System.out.println("Patika Store'a hoşgeldiniz.");
         System.out.println("Yeni açıldığımız için çok falza ürün mevcut değil :)\n"
-                           +"Anlayışınız için teşekkür ederiz.");
-        System.out.println("Bugün sizinle ben ilgileneceğim, nasıl yardımcı olabilirim.\n"
-                           +"Cep telefonları için 1'e, Notebooklar için lütfen 2'ye basın.");
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Yanıtınız:");
-        choice= scan.nextInt();
-        if (choice==1 || choice==2){
-            switch (choice){
-                case 1:
-                    System.out.println(phone1);
-                    System.out.println(phone2);
-                    System.out.println(phone3);
+                + "Anlayışınız için teşekkür ederiz."
+                + "Bugün sizinle ben ilgileneceğim, nasıl yardımcı olabilirim.\n");
 
+        while (true) {
+            String choice;
+            System.out.println("Cep telefonları için 1'e, Notebooklar için lütfen 2'ye basın.");
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("Ürün eklemek için 3'e, Ürün silmek için 4'e basın.");
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("Markaya göre ürün listelemek için 5'e, İd numarasına göre listelemek için 6'ya basın.");
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("Bütün ürünleri listelemek için lütfen 7'ye basın.");
+            System.out.println("--------------------------------------------------------------");
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Yanıtınız:");
+            choice = scan.nextLine();
+            switch (choice) {
+                case "1":
+                    listprintPhone(list);
                     break;
-                case 2:
-                    System.out.println(noteBook1);
-                    System.out.println(noteBook2);
-                    System.out.println(noteBook3);
-                    System.out.println(noteBook4);
-                    System.out.println(noteBook5);
-                    System.out.println(noteBook6);
+                case "2":
+                    listprintNoteBook(list);
                     break;
+                case "3":
+                    System.out.print("Id");
+                    int Id = scan.nextInt();
+                    System.out.print("Name");
+                    String name = scan.nextLine();
+                    System.out.println(" ");
+                    System.out.print("Model");
+                    String type = scan.nextLine();
+                    System.out.print("Tür");
+                    String model = scan.nextLine();
+                    System.out.print("Fiyat");
+                    int perPrice = scan.nextInt();
+                    System.out.print("İndirim");
+                    int sale = scan.nextInt();
+                    System.out.print("Stok");
+                    int stock = scan.nextInt();
+                    System.out.print("Hafıza");
+                    int rom = scan.nextInt();
+                    System.out.print("Ram");
+                    int ram = scan.nextInt();
+                    System.out.print("Pil");
+                    int battery = scan.nextInt();
+                    System.out.print("Ekran B.");
+                    double screenSize = scan.nextDouble();
+                    System.out.print("Renk");
+                    String color = "Mavi";
+                    Model addM = new Model(Id, name, type,model, perPrice, sale, stock, rom, ram, battery, screenSize, color);
+                    list.add(addM);
+
+                    listprint(list);
+                    break;
+                case "4":
+                    int id = 12345678;
+                    list = removeModel(id, list);
+                    listprint(list);
+                    break;
+                case "5":
+                    String marka = "Apple";
+                    List<Model> markaList = new ArrayList<>();
+                    markaList = getMarka(marka, list);
+                    listprint(markaList);
+                    break;
+                case "6":
+                    int id2 = 12345;
+                    System.out.println(getId(id2, list).toString());
+                    break;
+                case "7":
+                    listprint(list);
+
+                default:
+                    System.out.println("Yanlış değer girdiniz.");
+                    System.out.println("Tekrardan..");
+                    continue;
 
             }
-        }else {
-                System.out.println("Lütfen 1' veya 2'ye basınız.");
-                System.out.print("Yanıtınız:");
-                choice=scan.nextInt();
-
+            break;
         }
 
     }
+                    //Ürün silme fonkisyonu
+    public static List<Model> removeModel(int id, List<Model> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).id == id) {
+                list.remove(i);
+            }
+        }
+        return list;
+    }
+                    //Ürün id'lerini çekme fonksiyonu
+    public static Model getId(int id, List<Model> list) {
+        for (Model Model : list) {
+            if (Model.id == id) {
+                return Model;
+            }
+        }
+        return null;
+    }
+                    //Ürün markasını çekme fonksiyonu
+    public static List<Model> getMarka(String marka, List<Model> list) {
+        List<Model> markaList = new ArrayList<>();
+        for (Model Model : list) {
+            if (Model.name == marka) {
+                markaList.add(Model);
+            }
+        }
+        return markaList;
+    }
+                    //Ürün listeleme fonksiyonu
+    public static void listprint(List<Model> list) {
+        for (Model Model : list) {
+            System.out.println(Model.toString());
+            System.out.println("--------------------------------------------------");
+        }
+    }
+                    //Sadece telefonları listele
+    public static void listprintPhone(List<Model> list) {
+        for (Model Model : list) {
+            if (Model.model == "Phone") {
+                System.out.println(Model.toString());
+                System.out.println("--------------------------------------------------");
+            }
+        }
+    }
+                    //Sadece notebookları listele
+    public static void listprintNoteBook(List<Model> list) {
+        for (Model Model : list) {
+            if (Model.model == "Notebook") {
+                System.out.println(Model.toString());
+                System.out.println("--------------------------------------------------");
+            }
+        }
+    }
 }
+
+
--- src/Model.java
+++ src/Model.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
-public class MarkaModel {
-    public int Id;
-    public String Name;
-    public String Phone;
-    public String Notebook;
+public class Model {
+    public int id;
+    public String name;
+    public String type;
+   public String model;
     public int perPrice;
     public int sale;
     public int stock;
@@ -14,16 +13,17 @@ public class MarkaModel {
     public String color;
 
 
-    public MarkaModel(int id, String name, String phone) {
-        Id = id;
-        Name = name;
-        Phone = phone;
+    public Model(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
     }
 
-    public MarkaModel(int id, String name, String phone, int perPrice, int sale, int stock, int rom, int ram, int battery, double screenSize, String color) {
-        Id = id;
-        Name = name;
-        Phone = phone;
+    public Model(int id, String name, String type,String model, int perPrice, int sale, int stock, int rom, int ram, int battery, double screenSize, String color) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.model = model;
         this.perPrice = perPrice;
         this.sale = sale;
         this.stock = stock;
@@ -36,9 +36,9 @@ public class MarkaModel {
 
     @Override
     public String toString() {
-        return "İşte ürünlerimiz= " +
-                " Marka= " + Name +
-                ", Model= " + Phone +
+        return
+                " Marka= " + name +
+                ", Model= " + type +
                 ", Fiyat= " + perPrice +" TL" +
                 ", İndirim= %" + sale +
                 ", Stok adedi= " + stock +
